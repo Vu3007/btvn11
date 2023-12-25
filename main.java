b5c9dc8aa2e05817db1ddff7d347463e68679022
Vu3007 <148880693+Vu3007@users.noreply.github.com>
2023-12-25 20:52:47 +0700

    update

---
+++ main.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class main {
+    // Ghi Object JSON file (Object là 1 đối tượng bất kỳ : Có thể là Single Object hoặc List Object)
+    public <Gson> void convertObjectToJsonFile(String fileName, Object obj) {
+        try {
+            // Tạo đối tượng gson
+            // Gson gson = new Gson();
+
+            // Nếu muốn format JSON cho đẹp
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            // Tạo đối tượng Writer để ghi nội dung vào file
+            Writer writer = Files.newBufferedWriter(Paths.get(fileName));
+
+            // Ghi object vào file
+            gson.toJson(obj, writer);
+
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
